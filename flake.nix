220797b2ef0ab648ab6400bc73110cfd9cf37256
Jaan Jahilo <jaanjahilo@outlook.com>
2025-06-01 22:29:22 +0300

    Add nixfmt pre-commit hook and exclude hardware config

---
+++ flake.nix
@@ -0,0 +1,32 @@
+{
+  description = "Git hooks flake";
+
+  inputs.pre-commit-hooks.url = "github:cachix/git-hooks.nix";
+
+  outputs = { self, nixpkgs, ... }@inputs:
+    let
+      supportedSystems = [ "x86_64-linux" "aarch64-linux" "x86_64-darwin" "aarch64-darwin" ];
+
+      forAllSystems = nixpkgs.lib.genAttrs supportedSystems;
+    in
+    {
+      checks = forAllSystems (system: {
+        pre-commit-check = inputs.pre-commit-hooks.lib.${system}.run {
+          src = ./.;
+          hooks = {
+            nixpkgs-fmt = {
+              enable = true;
+              excludes = ["hardware-configuration\\.nix$"];
+            };
+          };
+        };
+      });
+
+      devShells = forAllSystems (system: {
+        default = nixpkgs.legacyPackages.${system}.mkShell {
+          inherit (self.checks.${system}.pre-commit-check) shellHook;
+          buildInputs = self.checks.${system}.pre-commit-check.enabledPackages;
+        };
+      });
+    };
+}
