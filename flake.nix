8a86585a2f1dac70bb090b3fcc6d4f83527ef4fb
Jaan Jahilo <jaanjahilo@outlook.com>
2025-06-02 21:28:45 +0300

    Add home.nix file

--- flake.nix
+++ flake.nix
@@ -1,22 +1,44 @@
 {
-  description = "Git hooks flake";
+  description = "jaan's NixOS configuration";
 
-  inputs.pre-commit-hooks.url = "github:cachix/git-hooks.nix";
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    home-manager = {
+      url = "github:nix-community/home-manager";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    pre-commit-hooks.url = "github:cachix/git-hooks.nix";
+  };
 
-  outputs = { self, nixpkgs, ... }@inputs:
+  outputs = { self, nixpkgs, home-manager, pre-commit-hooks, ... }@inputs:
     let
       supportedSystems = [ "x86_64-linux" "aarch64-linux" "x86_64-darwin" "aarch64-darwin" ];
-
       forAllSystems = nixpkgs.lib.genAttrs supportedSystems;
     in
     {
+      nixosConfigurations = {
+        nixos = nixpkgs.lib.nixosSystem {
+          system = "x86_64-linux";
+          specialArgs = { inherit inputs; };
+          modules = [
+            ./configuration.nix
+            home-manager.nixosModules.home-manager
+            {
+              home-manager.useGlobalPkgs = true;
+              home-manager.useUserPackages = true;
+              home-manager.users.jaan = import ./home.nix;
+            }
+          ];
+        };
+      };
+
       checks = forAllSystems (system: {
         pre-commit-check = inputs.pre-commit-hooks.lib.${system}.run {
           src = ./.;
           hooks = {
             nixpkgs-fmt = {
               enable = true;
-              excludes = ["hardware-configuration\\.nix$"];
+              excludes = [ "hardware-configuration\\.nix$" ];
             };
           };
         };
