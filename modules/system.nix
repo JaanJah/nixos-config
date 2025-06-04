df6ba205461d2b719921b7652d417be066ff2a60
Jaan Jahilo <jaanjahilo@outlook.com>
2025-06-04 07:21:49 +0300

    Add nix garbage collection

--- modules/system.nix
+++ modules/system.nix
@@ -1,4 +1,5 @@
 {
+  lib,
   pkgs,
   username,
   ...
@@ -26,6 +27,11 @@
   ];
 
   nix = {
+    gc = {
+      automatic = lib.mkDefault true;
+      dates = lib.mkDefault "weekly";
+      options = lib.mkDefault "--delete-older-than 7d";
+    };
     settings.experimental-features = [
       "nix-command"
       "flakes"
