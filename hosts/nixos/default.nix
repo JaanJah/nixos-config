6b034861c0737d20c8547e28745794e15a8f7242
Jaan Jahilo <jaanjahilo@outlook.com>
2025-06-04 05:12:14 +0300

    Add ublock-origin extension

--- hosts/nixos/default.nix
+++ hosts/nixos/default.nix
@@ -83,6 +83,7 @@
         listToAttrs [
           (extension "bitwarden-password-manager" "{446900e4-71c2-419f-a6a7-df9c091e268b}")
           (extension "tampermonkey" "firefox@tampermonkey.net")
+          (extension "ublock-origin" "uBlock0@raymondhill.net")
         ];
     };
   };
