92a2367f4282a5af6bd32554dab0bbd6891a0c3c
Jaan Jahilo <jaanjahilo@outlook.com>
2025-06-05 18:20:52 +0300

    Separate firefox to home/programs/firefox.nix

---
+++ home/programs/firefox.nix
@@ -0,0 +1,25 @@
+{ ... }:
+{
+  programs.firefox = {
+    enable = true;
+    policies = {
+      ExtensionSettings =
+        with builtins;
+        let
+          extension = shortId: uuid: {
+            name = uuid;
+            value = {
+              install_url = "https://addons.mozilla.org/en-US/firefox/downloads/latest/${shortId}/latest.xpi";
+              installation_mode = "normal_installed";
+            };
+          };
+        in
+        # Find id in about:support#addons
+        listToAttrs [
+          (extension "bitwarden-password-manager" "{446900e4-71c2-419f-a6a7-df9c091e268b}")
+          (extension "tampermonkey" "firefox@tampermonkey.net")
+          (extension "ublock-origin" "uBlock0@raymondhill.net")
+        ];
+    };
+  };
+}
