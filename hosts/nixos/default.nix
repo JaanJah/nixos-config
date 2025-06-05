1a4d779405e7c26cea57ff512d24377e46b280dc
Jaan Jahilo <jaanjahilo@outlook.com>
2025-06-05 18:11:53 +0300

    Split more of hosts/nixos to modules

--- hosts/nixos/default.nix
+++ hosts/nixos/default.nix
@@ -5,49 +5,22 @@
 { pkgs, ... }:
 {
   imports = [
+    ../../modules/audio.nix
+    ../../modules/boot.nix
     ../../modules/system.nix
+    ../../modules/network.nix
     ../../modules/nvidia.nix
     # Include the results of the hardware scan.
     ./hardware-configuration.nix
   ];
 
-  # Bootloader.
-  boot.loader.grub.enable = true;
-  boot.loader.grub.device = "/dev/sda";
-  boot.loader.grub.useOSProber = true;
-  boot.loader.timeout = 30; # https://github.com/NixOS/nixpkgs/blob/7282cb574e0607e65224d33be8241eae7cfe0979/nixos/modules/system/boot/loader/loader.nix
-
   networking.hostName = "nixos"; # Define your hostname.
-  # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
-
-  # Configure network proxy if necessary
-  # networking.proxy.default = "http://user:password@proxy:port/";
-  # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
-
-  # Enable networking
-  networking.networkmanager.enable = true;
 
   # Configure console keymap
   console.keyMap = "et";
 
   # Enable CUPS to print documents.
   services.printing.enable = true;
-
-  # Enable sound with pipewire.
-  services.pulseaudio.enable = false;
-  security.rtkit.enable = true;
-  services.pipewire = {
-    enable = true;
-    alsa.enable = true;
-    alsa.support32Bit = true;
-    pulse.enable = true;
-    # If you want to use JACK applications, uncomment this
-    #jack.enable = true;
-
-    # use the example session manager (no others are packaged yet so this is enabled by default,
-    # no need to redefine it in your config for now)
-    #media-session.enable = true;
-  };
 
   # Enable touchpad support (enabled default in most desktopManager).
   # services.xserver.libinput.enable = true;
@@ -63,7 +36,7 @@
       kdePackages.kate
     ];
   };
-  # TODO: Install firefox extensions
+
   programs.firefox = {
     enable = true;
     policies = {
@@ -127,16 +100,8 @@
   #   enableSSHSupport = true;
   # };
 
-  # List services that you want to enable:
-
   # Enable the OpenSSH daemon.
   # services.openssh.enable = true;
-
-  # Open ports in the firewall.
-  # networking.firewall.allowedTCPPorts = [ ... ];
-  # networking.firewall.allowedUDPPorts = [ ... ];
-  # Or disable the firewall altogether.
-  # networking.firewall.enable = false;
 
   # This value determines the NixOS release from which the default
   # settings for stateful data, like file locations and database versions
@@ -145,5 +110,4 @@
   # Before changing this value read the documentation for this option
   # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
   system.stateVersion = "25.05"; # Did you read the comment?
-
 }
---
+++ modules/boot.nix
@@ -0,0 +1,14 @@
+{ ... }:
+{
+  boot = {
+    loader = {
+      grub = {
+        enable = true;
+        device = "/dev/sda";
+        useOSProber = true;
+      };
+      # https://github.com/NixOS/nixpkgs/blob/7282cb574e0607e65224d33be8241eae7cfe0979/nixos/modules/system/boot/loader/loader.nix
+      timeout = 30;
+    };
+  };
+}
