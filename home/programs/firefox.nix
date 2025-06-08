f520fce11514ea736c29790b7f376453c4628ff2
Jaan Jahilo <jaanjahilo@outlook.com>
2025-06-08 18:22:22 +0300

    Add imagus extension to Firefox

--- home/programs/firefox.nix
+++ home/programs/firefox.nix
@@ -17,6 +17,7 @@
         # Find id in about:support#addons
         listToAttrs [
           (extension "bitwarden-password-manager" "{446900e4-71c2-419f-a6a7-df9c091e268b}")
+          (extension "imagus" "{00000f2a-7cde-4f20-83ed-434fcb420d71}")
           (extension "tampermonkey" "firefox@tampermonkey.net")
           (extension "ublock-origin" "uBlock0@raymondhill.net")
         ];
