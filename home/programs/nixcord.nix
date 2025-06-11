ef73dd534919dcac2d07634a68feb9cdfc4fe1a0
Jaan Jahilo <jaanjahilo@outlook.com>
2025-06-11 20:14:37 +0300

    Add some vencord plugins

--- home/programs/nixcord.nix
+++ home/programs/nixcord.nix
@@ -11,6 +11,7 @@
     config = {
       # https://github.com/KaylorBen/nixcord/blob/cc750b99c76c83809cb7e492cab6930c610faa64/docs/plugins.md
       plugins = {
+        # Defaults, don't touch these unless you know what you're doing
         chatInputButtonAPI.enable = true;
         commandsAPI.enable = true;
         crashHandler.attemptToPreventCrashes = true;
@@ -23,6 +24,29 @@
         messageUpdaterAPI.enable = true;
         serverListAPI.enable = true;
         userSettingsAPI.enable = true;
+
+        # Plugins
+        copyFileContents.enable = true;
+        emoteCloner.enable = true;
+        experiments.enable = true;
+        fixSpotifyEmbeds.enable = true;
+        fixYoutubeEmbeds.enable = true;
+        friendsSince.enable = true;
+        imageZoom.enable = true;
+        messageLogger.enable = true;
+        noOnboardingDelay.enable = true;
+        noUnblockToJump.enable = true;
+        permissionsViewer.enable = true;
+        showHiddenChannels.enable = true;
+        sortFriendRequests.enable = true;
+        showHiddenThings.enable = true;
+        showTimeoutDuration.enable = true;
+        spotifyCrack.enable = true;
+        translate.enable = true;
+        validReply.enable = true;
+        validUser.enable = true;
+        whoReacted.enable = true;
+        youtubeAdblock.enable = true;
       };
     };
   };
