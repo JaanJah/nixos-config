3c1b31da12a9b30be75c156cd5006182fbccb0a2
Jaan Jahilo <jaanjahilo@outlook.com>
2025-06-11 19:52:36 +0300

    Switch to use vesktop

--- home/programs/nixcord.nix
+++ home/programs/nixcord.nix
@@ -2,5 +2,19 @@
   # https://github.com/KaylorBen/nixcord/blob/cc750b99c76c83809cb7e492cab6930c610faa64/docs/INDEX.md
   programs.nixcord = {
     enable = true;
+    discord = {
+      enable = false;
+    };
+    vesktop = {
+      enable = true;
+    };
+    config = {
+      plugins = {
+        crashHandler = {
+          enable = true;
+          attemptToPreventCrashes = true;
+        };
+      };
+    };
   };
 }
