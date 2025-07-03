cf155ef693b9f2667bedd4925235befb48767980
Jaan Jahilo <jaanjahilo@outlook.com>
2025-07-03 09:27:26 +0300

    Manage python versions with uv

--- home/programs/uv.nix
+++ home/programs/uv.nix
@@ -4,8 +4,8 @@
     enable = true;
     # https://docs.astral.sh/uv/reference/settings
     settings = {
-      python-downloads = "never";
-      python-preference = "only-system";
+      python-downloads = "automatic";
+      python-preference = "managed";
     };
   };
 }
