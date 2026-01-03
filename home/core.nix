2c4d706d6befc7978bd5dde04b0011cc475da4eb
Jaan Jahilo <jaanjahilo@outlook.com>
2026-01-03 12:35:08 +0200

    Install python3.15 and python3.14 as fallback

--- home/core.nix
+++ home/core.nix
@@ -14,8 +14,11 @@
     sessionVariables = {
       NPM_CONFIG_PREFIX = "${config.home.homeDirectory}/.npm-global";
     };
+    # Read more about lowPrio: https://nixos.org/manual/nixpkgs/stable/#function-library-lib.meta.lowPrio
     packages = with pkgs; [
       nodejs_24
+      python315
+      (lib.meta.lowPrio python314)
     ];
     stateVersion = "25.05";
   };
