55cb0168bee4ae3903ec876f72629558888b95ba
Jaan Jahilo <jaanjahilo@outlook.com>
2026-02-16 16:09:25 +0200

    Remove cyberghost vpn

--- modules/openvpn.nix
+++ modules/openvpn.nix
@@ -1,17 +1,4 @@
 {
   # https://wiki.nixos.org/wiki/OpenVPN
-  services.openvpn.servers = {
-    # https://my.cyberghostvpn.com/products-vpn/manage-devices
-    # sudo systemctl start openvpn-cyberghost-sweden.service
-    # systemctl status openvpn-cyberghost-sweden.service
-    # sudo systemctl stop openvpn-cyberghost-sweden.service
-    cyberghost-sweden = {
-      config = ''config /etc/nixos/openvpn/cyberghost-sweden/openvpn.ovpn '';
-      autoStart = false;
-    };
-    cyberghost-usa = {
-      config = ''config /etc/nixos/openvpn/cyberghost-usa/openvpn.ovpn '';
-      autoStart = false;
-    };
-  };
+  services.openvpn.servers = { };
 }
