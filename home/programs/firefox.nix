38defb6114cdc24cfe6226f53d8cfe0a69ca3e95
Jaan Jahilo <jaanjahilo@outlook.com>
2026-03-08 09:01:30 +0200

    Add steamlevels-steam-enhancer browser addon

--- home/programs/firefox.nix
+++ home/programs/firefox.nix
@@ -20,6 +20,7 @@
           (extension "tampermonkey" "firefox@tampermonkey.net")
           (extension "ublock-origin" "uBlock0@raymondhill.net")
           (extension "web-scrobbler" "{799c0914-748b-41df-a25c-22d008f9e83f}")
+          (extension "steamlevels-steam-enhancer" "{27ef8309-55ec-435e-9447-e8d3308b965a}")
         ];
     };
   };
